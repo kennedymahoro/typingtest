a39ed4c326255ea7dbb64c435a1170fc6ecfe69b
Kennedy Mahoro <106330299+kennedymahoro@users.noreply.github.com>
2024-10-23 15:55:57 -0400

    updated navbar, link to github

--- app/components/NavbarComp.tsx
+++ app/components/NavbarComp.tsx
@@ -1,24 +1,24 @@
 "use client";
 import { Navbar, DarkThemeToggle } from "flowbite-react";
 export default function NavbarComp() {
-    return (
+  return (
     <Navbar fluid rounded className="w-screen">
-        <Navbar.Brand href="https://flowbite.com/">
-            <span className="self-center whitespace-nowrap text-xl font-semibold dark:text-white">
-            Typing Test
-            </span>
-        </Navbar.Brand>
-        <Navbar.Toggle />
-        <Navbar.Collapse>
-            <Navbar.Link href="/navbars" active>
-            Home
-            </Navbar.Link>
-            <Navbar.Link href="/navbars">About</Navbar.Link>
-            <Navbar.Link href="/navbars">Services</Navbar.Link>
-            <Navbar.Link href="/navbars">Pricing</Navbar.Link>
-            <Navbar.Link href="/navbars">Contact</Navbar.Link>
-        </Navbar.Collapse>
-        <DarkThemeToggle />
+      <Navbar.Brand href="/">
+        <span className="self-center whitespace-nowrap text-xl font-semibold dark:text-white">
+          Typing Test
+        </span>
+      </Navbar.Brand>
+      <Navbar.Toggle />
+      <Navbar.Collapse>
+        <Navbar.Link href="/navbars" active>
+          Home
+        </Navbar.Link>
+        <Navbar.Link href="/navbars">Typing Test</Navbar.Link>
+        <Navbar.Link href="https://github.com/kennedymahoro">
+          GitHub
+        </Navbar.Link>
+      </Navbar.Collapse>
+      <DarkThemeToggle />
     </Navbar>
-    )
+  );
 }
