5a59db86d1c1e9bdcd651ce8634f23469b83b244
Kennedy Mahoro <106330299+kennedymahoro@users.noreply.github.com>
2024-10-25 12:26:46 -0400

    handle event change, initializing

--- app/Test/page.tsx
+++ app/Test/page.tsx
@@ -1,6 +1,31 @@
+'use client';
 import NavbarComp from "../components/NavbarComp";
 import { Textarea } from "flowbite-react";
+import { HtmlHTMLAttributes, useState } from "react";
 export default function Test() {
+    interface spanArray {
+        type: HtmlHTMLAttributes
+    }
+    // textarea logic function
+    // for handling text
+    const [text, setText] = useState("");
+    // from handling quote
+    const [quote, setQuote] = useState("");
+    // for handling <span> tag array of quote
+    const [quoteArray, setQuoteArray] = useState<string []| void[]>();
+    // for handling time
+    const [time, setTime] = useState(0);
+
+    const handleStart = () => {
+        setQuote("Typing is an important skill for anyone using a computer. The more comfortable you are typing, the more you can focus on what you are working on. Slow typing and fixing typing errors moves your attention away from what you are trying to achieve (writing an email, filling in a form, etc.). Use this advanced typing test to discover how well you type. We have test material in more than 15 languages.");
+        setQuoteArray(quote.split("").map((char) => {<span>{char}</span>}));
+
+    }
+
+
+    const handleTyping = (e) => {
+
+    }
   return (
     <div className="flex h-screen w-screen flex-col dark:bg-gray-800 dark:text-gray-400">
       <NavbarComp />
@@ -8,16 +33,11 @@ export default function Test() {
         <div className="p-3.5 py-28">
             <h5 className="text-xl font-bold dark:text-white"> Quote</h5>
             <p className="p-4">
-            Typing is an important skill for anyone using a computer. The more
-            comfortable you are typing, the more you can focus on what you are
-            working on. Slow typing and fixing typing errors moves your attention
-            away from what you are trying to achieve (writing an email, filling in
-            a form, etc.). Use this advanced typing test to discover how well you
-            type. We have test material in more than 15 languages.
+                {quote}
             </p>
         </div>
         <div className="p-3.5 justify-items-center">
-            <Textarea placeholder="Type here..." className="max-h-10 w-56" />
+            <Textarea placeholder="Type here..." className="max-h-10 w-56"  onChange={handleTyping} />
         </div>
       </div>
     </div>
